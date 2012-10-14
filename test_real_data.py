ac8642ebd171721f52ad9e4303333c4923907aac
Fela Winkelmolen <felawinkelmolen@gmail.com>
2012-10-14 11:27:02 +0200

    minor code cleanup

--- test_real_data.py
+++ test_real_data.py
@@ -16,14 +16,11 @@ import matrixdecomposition as md
 #############################################################
 class Experiment:
     def __init__(self, **opt):
-        self.filename = opt.get('filename') or 'SMALL'
-        self.seed = opt.get('seed') or 0
-        self.holes = opt.get('holes') or 0.0 # holes percentage
-        self.completion = opt.get('completion') or 'matrix'
-        opt.pop('filename', None)
-        opt.pop('seed', None)
-        opt.pop('holes', None)
-        opt.pop('completion', None)
+        self.filename = opt.pop('filename', None) or 'SMALL'
+        self.seed = opt.pop('seed', None) or 0
+        self.holes = opt.pop('holes', None) or 0.0 # holes percentage
+        self.completion = opt.pop('completion', None) or 'matrix'
+        
         self.options = opt
         self.generate_matrix()
 
@@ -64,7 +61,7 @@ class Experiment:
                 else:
                     means[c] = sum/n
             
-            print means
+            #print means
             res = double(self.Y)
             for r in range(rows):
                 for c in range(cols):
@@ -77,8 +74,6 @@ class Experiment:
     def generate_matrix(self):
         filename = 'data/' + self.filename + '.train'
         self.TH, _ = self.load_data(filename)
-        print '---------------------------'        
-        print sp.linalg.svd(self.TH)
         self.GA = self.TH * 0
         self.Y = self.TH + self.GA
     
@@ -98,11 +93,10 @@ class Experiment:
         X = M[:, range(1, M.shape[1])]
         return (X, y)
 
-def run_experiment(**opt):
-    n = opt.get('steps') or 5
-    runs = opt.get('runs') or 1
-    opt.pop('steps', None)
-    opt.pop('runs', None)
+def hole_experiment(**opt):
+    n = opt.pop('steps', None) or 5
+    runs = opt.pop('runs', None) or 1
+    label = opt.pop('label', None)
     #opt['mu_d'] = 0
     y = sp.array(range(n+1)) / float(n)
     x = []
@@ -114,11 +108,12 @@ def run_experiment(**opt):
             e.generate_mask(holes)
             acc.append(e.run())
         x.append(sp.array(acc).mean())
-    lbl = str(runs)
-    print x
-    plot(y, x, label=lbl)
+    plot(y, x, label=label)
     legend(loc=0)
+        
+        
+    
 
 
 # example experiment
-run_experiment(steps=5, alpha=100000, mu_d=1, completion='matrix', seed=10)
+hole_experiment(steps=15, alpha=100000, mu_d=1, completion='mean', seed=10, label='mean')
